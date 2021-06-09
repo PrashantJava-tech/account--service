7524660f8cee30bda03aec2840ceb3a059cbea26
PrashantJava-tech <prashant.kdm29@gmail.com>
2021-06-09 21:47:58 +0530

    yaml file update

--- src/main/java/com/AccountService/AccountService/AccountServiceApplication.java
+++ src/main/java/com/AccountService/AccountService/AccountServiceApplication.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
-@EnableDiscoveryClient
+@EnableEurekaClient
+//@EnableDiscoveryClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.name", "account-server");
+		//System.setProperty("spring.config.name", "account-server");
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
 
